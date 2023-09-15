1d8c4dee79672340118e7cfcc4e1902faff6f9cb
d0wnsider <jmhandsamson@gmail.com>
2023-09-15 00:30:35 -0400

    finish fib seq recursion

--- Assignment 1/fibs.js
+++ Assignment 1/fibs.js
@@ -8,3 +8,11 @@ function fibs(num) {
 }
 
 fibs(8);
+
+function fibsRec(n) {
+  if (n === 2) return [0, 1];
+
+  return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
+
+console.log(fibsRec(8));
